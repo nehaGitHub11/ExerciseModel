9cc1ae50823638fcdffb9dc5bfefb6ddc012c450
nehaGitHub11 <neha.sonepat11@gmail.com>
2020-08-10 14:55:41 +1000

    * added trolley calculator class

---
+++ Trolley.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NehaExerciseModel
+{
+    public class Trolley
+    {
+        public List<Product> products { get; set; }
+
+        public List<Specials> specials { get; set; }
+
+        public List<Quantities> quantities { get; set; }
+    }
+
+    public class Quantities
+    { 
+    public string name { get; set; }
+
+    public decimal quantity { get; set; }
+    }
+
+    public class Specials
+    {
+        public List<Quantities>  quantities { get; set; }
+
+        public decimal total { get; set; }
+    }
+}
